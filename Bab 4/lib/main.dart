f6e75cc194c992fd6c118f54b0faa1d4702de382
Jabar <violetta.je.vymmiy15@gmail.com>
2023-10-29 20:54:06 +0700

    bab 4

---
+++ Bab 4/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:bab_4_mcs/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'MainPage.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'mcs bab4',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.dark(),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
